324bdf2f82ad7b64af22fa1d2a830835cb235aa0
Pedro <pedroceolato2002@gmail.com>
2022-11-15 20:57:43 -0300

    edit name of drive

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import cors from '@fastify/cors';
 import jwt from '@fastify/jwt';
 
 import { poolRoutes } from './routes/pool';
-import { userRoutes } from './routes/user,';
+import { userRoutes } from './routes/user';
 import { guessRoutes } from './routes/guess';
 import { authRoutes } from './routes/auth';
 import { gameRoutes } from './routes/game';
@@ -28,7 +28,7 @@ async function bootstrap() {
     await fastify.register(guessRoutes) 
     await fastify.register(userRoutes)
 
-    await fastify.listen({ port: 3333, /* host: '0.0.0.0' */ })
+    await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
